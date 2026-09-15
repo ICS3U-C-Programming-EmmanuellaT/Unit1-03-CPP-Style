aa3481b86baeae2f0dfdbb3ce4193bbb703066b5
Emmanuella Taiwo <emmanuella.taiwo@stu.ocsb.ca>
2026-09-15 15:48:27 +0000

    first commit of helloWorldstyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Emmanuella Taiwo All rights reserved.
+// .
+// Created by: Emmanuella Taiwo
+// Date:Sep 15th,2026
+// This is a reading with proper style .
+
+#include <iostream>
+int main() {
+    std::cout << "Hello, Ms Raffin!" << std::endl;
+    std::cout << "This class is so much fun !" << std::endl;
+}
